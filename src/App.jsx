1df7614c3675b910f4d9f0cc9029082820b22145
vikas09rathour <vrnuts09@gmail.com>
2026-05-22 00:31:07 +0530

    fixed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 
 import Todo from"./todo";
 
-
+function App(){
   return (
        <div>
         <h1>TO Do APP</h1>
         <Todo/>
        </div>
            
-  )
+  );
+}
 
     
 
 
-export default App
+export default App;
